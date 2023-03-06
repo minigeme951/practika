6647b50e1752c9404fd4d4fb3dc2dd4eddd7632d
minigeme951 <denistato4enko707@gmail.com>
2023-03-06 15:11:26 +0600

    auth user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,6 +20,10 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
+    public function username()
+    {
+        return 'login';
+    }
 
     /**
      * Where to redirect users after login.
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,12 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="login" class="col-md-4 col-form-label text-md-end">{{ __('Login') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ old('login') }}" required autocomplete="login" autofocus>
 
-                                @error('email')
+                                @error('login')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
