b71ab6e931f4cef2f967500dc221e7be0967b040
minigeme951 <denistato4enko707@gmail.com>
2023-03-09 16:08:51 +0600

    add button add cart

--- resources/views/cataloglist.blade.php
+++ resources/views/cataloglist.blade.php
@@ -53,7 +53,18 @@
                         <div class="card-body">
                             <h5 class="card-title">{{$obprod->name}}</h5>
                             <p class="card-text">Цена:{{$obprod->price}}руб</p>
+                            <div class="btn-group">
                             <a href="{{url('/catalog/product')}}/{{$obprod->id}}" class="btn btn-primary">Подробнее</a>
+                                @if (auth()->check())
+                                    @if($obprod->count>0)
+                                        <a href="{{Route('cartAdd', $obprod->id) }}" class="btn btn-primary">Добавить в корзину</a>
+                                    @else
+                                        <a class="btn btn-primary disabled" href="">НЕТ В НАЛИЧИИ</a>
+                                    @endif
+                                @else
+                                    <a href="{{url('/login')}}"class="btn btn-info">Авторизируйтесь</a>
+                                @endif
+                            </div>
                         </div>
                     </div>
                 </div>
